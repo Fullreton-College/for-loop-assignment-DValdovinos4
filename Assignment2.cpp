d14d6a90ad431dd6c81feae05e7d556b58585a7c
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-03-02 15:52:40 -0800

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-int main() {
+const int NumStores= 5;
+const int AsteriskValue= 100;
 
-   /* Type your code here. */
+int main()
+{
+    int sales[NumStores];
 
-   return 0;
+    for(int i=0; i<NumStores; i++){
+        cout<<"Enter today'sales for store " << (i+1) << ": ";
+        cin>>sales[i];
+    
+    }
+    cout << "\nSALES BAR CHART" <<endl;
+    cout << "(Each * =$" << AsteriskValue << ")\n";
+
+    for(int i=0; i<NumStores; i++)
+    {
+        cout<<"Store "<< (i+1) << ":";
+        int numAsterisks = sales[i] / AsteriskValue;
+        for(int j=0; j<numAsterisks; j++)
+        {
+            cout<<"*";
+        }
+    }
+ return 0;
 }
