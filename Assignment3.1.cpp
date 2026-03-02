9b31d683abaf98b0ce6056778986f18673dc170c
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-03-02 15:53:22 -0800

    Update Assignment3.1.cpp

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,31 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
+
+const int NumStores= 5;
+const int AsteriskValue= 100;
 
 int main()
 {
+    int sales[NumStores];
 
-return 0;
+    for(int i=0; i<NumStores; i++){
+        cout<<"Enter today'sales for store " << (i+1) << ": ";
+        cin>>sales[i];
+    
+    }
+    cout << "\nSALES BAR CHART" <<endl;
+    cout << "(Each * =$" << AsteriskValue << ")\n";
+
+    for(int i=0; i<NumStores; i++)
+    {
+        cout<<"Store "<< (i+1) << ":";
+        int numAsterisks = sales[i] / AsteriskValue;
+        for(int j=0; j<numAsterisks; j++)
+        {
+            cout<<"*";
+        }
+    }
+ return 0;
 }
